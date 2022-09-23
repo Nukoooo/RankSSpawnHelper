ea76df7ba51a2f912f9b2f453e38317c652d23e9
NukoOoOoOoO <nukoooo@nukooo.cc>
2022-09-23 22:18:42 +0800

    1.2.0.1

--- RankSSpawnHelper/DalamudApi.cs
+++ RankSSpawnHelper/DalamudApi.cs
@@ -10,33 +10,34 @@ using Dalamud.Game.Gui;
 using Dalamud.Game.Gui.Dtr;
 using Dalamud.IoC;
 using Dalamud.Plugin;
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Local
 
 namespace RankSSpawnHelper
 {
     internal class DalamudApi
     {
-        [PluginService] internal static DalamudPluginInterface Interface { get; } = null!;
+        [PluginService] internal static DalamudPluginInterface Interface { get; private set; } = null!;
 
-        [PluginService] internal static ChatGui ChatGui { get; } = null!;
+        [PluginService] internal static ChatGui ChatGui { get; private set; } = null!;
 
-        [PluginService] internal static ClientState ClientState { get; } = null!;
+        [PluginService] internal static ClientState ClientState { get; private set; } = null!;
 
-        [PluginService] internal static CommandManager CommandManager { get; } = null!;
+        [PluginService] internal static CommandManager CommandManager { get; private set; } = null!;
 
-        [PluginService] internal static SigScanner SigScanner { get; } = null!;
+        [PluginService] internal static SigScanner SigScanner { get; private set; } = null!;
 
-        [PluginService] internal static DataManager DataManager { get; } = null!;
+        [PluginService] internal static DataManager DataManager { get; private set; } = null!;
 
-        [PluginService] internal static FateTable FateTable { get; } = null!;
+        [PluginService] internal static FateTable FateTable { get; private set; } = null!;
 
-        [PluginService] internal static ObjectTable ObjectTable { get; } = null!;
+        [PluginService] internal static ObjectTable ObjectTable { get; private set; } = null!;
 
-        [PluginService] internal static Framework Framework { get; } = null!;
+        [PluginService] internal static Framework Framework { get; private set; } = null!;
 
-        [PluginService] internal static Condition Condition { get; } = null!;
+        [PluginService] internal static Condition Condition { get; private set; } = null!;
 
-        [PluginService] internal static PartyList PartyList { get; } = null!;
+        [PluginService] internal static PartyList PartyList { get; private set; } = null!;
 
-        [PluginService] internal static DtrBar DtrBar { get; } = null!;
+        [PluginService] internal static DtrBar DtrBar { get; private set; } = null!;
     }
 }
