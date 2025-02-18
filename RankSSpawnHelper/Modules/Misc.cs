af1b7ec8f96d0a511415d319a25f68cb9b7d730e
Nuko <me@nuko.me>
2025-02-18 17:29:34 +0800

    a

--- RankSSpawnHelper/Modules/Misc.cs
+++ RankSSpawnHelper/Modules/Misc.cs
@@ -147,38 +147,34 @@ internal class Misc : IUiModule
         {
             DalamudApi.PluginLog.Error("Failed to get address #1");
 
-            return false;
-        }
+            if (!SafeMemory.ReadBytes(_address1 + 2, 2, out _bytes1))
+            {
+                DalamudApi.PluginLog.Error("Failed to read bytes #1");
 
-        if (!SafeMemory.ReadBytes(_address1 + 2, 2, out _bytes1))
-        {
-            DalamudApi.PluginLog.Error("Failed to read bytes #1");
+                return false;
+            }
 
-            return false;
+            if (_bytes1[0] == 0xF4)
+            {
+                _bytes1[0] = 0xF5;
+            }
         }
 
         if (!DalamudApi.SigScanner.TryScanText("83 F8 ?? 73 ?? 44 8B C0 1B D2", out _address2))
         {
             DalamudApi.PluginLog.Error("Failed to get address #2");
 
-            return false;
-        }
+            if (!SafeMemory.ReadBytes(_address2, 5, out _bytes2))
+            {
+                DalamudApi.PluginLog.Error("Failed to read bytes #2");
 
-        if (!SafeMemory.ReadBytes(_address2, 5, out _bytes2))
-        {
-            DalamudApi.PluginLog.Error("Failed to read bytes #2");
+                return false;
+            }
 
-            return false;
-        }
-
-        if (_bytes1[0] == 0xF4)
-        {
-            _bytes1[0] = 0xF5;
-        }
-
-        if (_bytes2[0] == 0x90)
-        {
-            _address2 = 0;
+            if (_bytes2[0] == 0x90)
+            {
+                _address2 = 0;
+            }
         }
 
         PatchWorldTravelQueue(_configuration.AccurateWorldTravelQueue);
@@ -390,12 +386,28 @@ internal class Misc : IUiModule
 
         Widget.BeginFramedGroup("杂项", new Vector2(-1, -1));
 
-        var worldTravelQueue = _configuration.AccurateWorldTravelQueue;
+        var isValid = _address1 != nint.Zero && _address2 != nint.Zero;
 
-        if (ImGui.Checkbox("显示实际跨服人数", ref worldTravelQueue))
         {
-            _configuration.AccurateWorldTravelQueue = worldTravelQueue;
-            _configuration.Save();
+            using var disable          = ImRaii.Disabled(!isValid);
+            var       worldTravelQueue = _configuration.AccurateWorldTravelQueue;
+
+            if (ImGui.Checkbox("显示实际跨服人数", ref worldTravelQueue))
+            {
+                _configuration.AccurateWorldTravelQueue = worldTravelQueue;
+                _configuration.Save();
+            }
+        }
+
+        if (!isValid)
+        {
+            ImGui.SameLine();
+            ImGui.TextColored(ImGuiColors.DalamudGrey, "(?)");
+
+            if (ImGui.IsItemHovered())
+            {
+                ImGui.SetTooltip("无法使用，可能因为和ACT的插件有冲突");
+            }
         }
 
         ImGui.SameLine();
@@ -869,6 +881,11 @@ internal class Misc : IUiModule
 
     private void PatchWorldTravelQueue(bool enabled)
     {
+        if (_address1 == nint.Zero || _address2 == nint.Zero)
+        {
+            return;
+        }
+
         if (enabled)
         {
             SafeMemory.WriteBytes(_address1 + 2, [0xF4, 0x30]);
